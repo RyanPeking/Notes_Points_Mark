ff986bebc9c75fea28680abc51530a040be6ef25
Ryan Wu <wsy2008401>
2019-12-17 16:52:32 +0800

    multilabel

--- MultiLabel/run_classifier.py
+++ MultiLabel/run_classifier.py
@@ -82,7 +82,7 @@ flags.DEFINE_integer(
 flags.DEFINE_bool("do_train", True, "Whether to run training.")
 
 # flags.DEFINE_bool("do_eval", False, "Whether to run eval on the dev set.")
-flags.DEFINE_bool("do_eval", True, "Whether to run eval on the dev set.")
+flags.DEFINE_bool("do_eval", False, "Whether to run eval on the dev set.")
 
 flags.DEFINE_bool(
     "do_predict", False,
@@ -407,25 +407,117 @@ class MyProcessor(DataProcessor):
 
   def get_labels(self):
     """See base class."""
-    return ["0", "1", "2"]
+    return ['高中',
+            '生物',
+            '地理',
+            '历史',
+            '政治',
+            '“重农抑商”政策',
+             '不完全显性',
+             '与细胞分裂有关的细胞器',
+             '中央官制——三公九卿制',
+             '中心体的结构和功能',
+             '人体免疫系统在维持稳态中的作用',
+             '人体水盐平衡调节',
+             '人体的体温调节',
+             '人口与城市',
+             '人口增长与人口问题',
+             '人口迁移与人口流动',
+             '人工授精、试管婴儿等生殖技术',
+             '伴性遗传',
+             '体液免疫的概念和过程',
+             '免疫系统的功能',
+             '免疫系统的组成',
+             '公民道德与伦理常识',
+             '兴奋在神经元之间的传递',
+             '兴奋在神经纤维上的传导',
+             '内环境的稳态',
+             '内质网的结构和功能',
+             '农业区位因素',
+             '减数分裂与有丝分裂的比较',
+             '减数分裂的概念',
+             '分子与细胞',
+             '劳动就业与守法经营',
+             '古代史',
+             '器官移植',
+             '地球与地图',
+             '地球所处的宇宙环境',
+             '地球的内部圈层结构及特点',
+             '地球的外部圈层结构及特点',
+             '地球运动的地理意义',
+             '地球运动的基本形式',
+             '垄断组织的出现',
+             '培养基与无菌技术',
+             '基因工程的原理及技术',
+             '基因工程的概念',
+             '基因的分离规律的实质及应用',
+             '基因的自由组合规律的实质及应用',
+             '复等位基因',
+             '夏商两代的政治制度',
+             '太阳对地球的影响',
+             '宇宙中的地球',
+             '工业区位因素',
+             '拉马克的进化学说',
+             '文艺的春天',
+             '核糖体的结构和功能',
+             '海峡两岸关系的发展',
+             '液泡的结构和功能',
+             '清末民主革命风潮',
+             '溶酶体的结构和功能',
+             '激素调节',
+             '现代史',
+             '现代生物技术专题',
+             '生产活动与地域联系',
+             '生命活动离不开细胞',
+             '生态系统的营养结构',
+             '生活中的法律常识',
+             '生物工程技术',
+             '生物性污染',
+             '生物技术在其他方面的应用',
+             '生物技术实践',
+             '生物科学与社会',
+             '皇帝制度',
+             '社会主义市场经济的伦理要求',
+             '社会主义是中国人民的历史性选择',
+             '神经调节和体液调节的比较',
+             '科学社会主义常识',
+             '稳态与环境',
+             '第三产业的兴起和“新经济”的出现',
+             '组成细胞的化合物',
+             '组成细胞的化学元素',
+             '细胞大小与物质运输的关系',
+             '细胞有丝分裂不同时期的特点',
+             '细胞的多样性和统一性',
+             '经济学常识',
+             '群落的结构',
+             '胚胎移植',
+             '蛋白质的合成',
+             '血糖平衡的调节',
+             '走进细胞',
+             '近代史',
+             '选官、用官制度的变化',
+             '遗传与进化',
+             '遗传的分子基础',
+             '遗传的细胞基础',
+             '避孕的原理和方法',
+             '郡县制',
+             '高尔基体的结构和功能'
+            ]
 
   def _create_examples(self, lines, set_type):
     """Creates examples for the training and dev sets."""
     examples = []
-    labels = []
-    print(len(lines))
     for (i, line) in enumerate(lines):
         # Only the test set has a header
         if set_type == "test" and i == 0:
             continue
         guid = "%s-%s" % (set_type, i)
         if set_type == "test":
-            text_a = tokenization.convert_to_unicode(line[1])
+            text_a = tokenization.convert_to_unicode(line[-1])
             label = "0"
         else:
-            text_a = tokenization.convert_to_unicode(line[1])
-            label = tokenization.convert_to_unicode(line[0])
-            labels.append(label)
+            text_a = tokenization.convert_to_unicode(line[-1])
+            label = tokenization.convert_to_unicode(' '.join(line[:-1]))
         examples.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
     return examples
 
